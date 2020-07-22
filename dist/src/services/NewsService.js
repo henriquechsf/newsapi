83f546751a74f85c2b8e4984a591ba1710801acf
Carlos Henrique S. Ferreira <ch.ferreir@hotmail.com>
2020-07-21 21:11:15 -0300

    add methods crud in NewsService.ts

--- dist/src/services/NewsService.js
+++ dist/src/services/NewsService.js
@@ -11,5 +11,14 @@ class NewsService {
     getById(_id) {
         return NewsRepository_1.default.findById(_id);
     }
+    create(news) {
+        return NewsRepository_1.default.create(news);
+    }
+    update(_id, news) {
+        return NewsRepository_1.default.findByIdAndUpdate(_id, news);
+    }
+    delete(_id, news) {
+        return NewsRepository_1.default.findByIdAndRemove(_id);
+    }
 }
 exports.default = new NewsService();
