860a0f9386caafa2f67cfae09d87659efb79b0fe
Carlos Henrique S. Ferreira <ch.ferreir@hotmail.com>
2020-07-22 20:50:39 -0300

    apply Docker in project

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -2,7 +2,7 @@ import * as mongoose from 'mongoose'
 
 class Database {
 
-    private DB_URL = 'mongodb://localhost:28017/db_portal'
+    private DB_URL = 'mongodb://link-db/db_portal'
 
     createConnection() {
         mongoose.connect(this.DB_URL)
