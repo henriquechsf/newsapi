ee4a7f167ee4611a9fbb10d1333b405e14e0552d
Carlos Henrique S. Ferreira <ch.ferreir@hotmail.com>
2020-07-25 21:36:38 -0300

    refact NewsService methods to async await

--- src/services/NewsService.ts
+++ src/services/NewsService.ts
@@ -5,24 +5,24 @@
 import NewsRepository from '../repositories/NewsRepository'
 
 class NewsService {
-    get() {
-        return NewsRepository.find({})
+    async get() {
+        return await NewsRepository.find({})
     }
 
-    getById(_id) {
-        return NewsRepository.findById(_id)
+    async getById(_id) {
+        return await NewsRepository.findById(_id)
     }
 
-    create(news) {
-        return NewsRepository.create(news)
+    async create(news) {
+        return await NewsRepository.create(news)
     }
 
-    update(_id, news) {
-        return NewsRepository.findByIdAndUpdate(_id, news)
+    async update(_id, news) {
+        return await NewsRepository.findByIdAndUpdate(_id, news)
     }
 
-    delete(_id) {
-        return NewsRepository.findByIdAndRemove(_id)
+    async delete(_id) {
+        return await NewsRepository.findByIdAndRemove(_id)
     }
 }
 
