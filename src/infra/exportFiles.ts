1b950dd1598fee5b50212b951d6bfd5904aaac19
Carlos Henrique S. Ferreira <ch.ferreir@hotmail.com>
2020-07-25 21:33:02 -0300

    implemented class to export files csv

---
+++ src/infra/exportFiles.ts
@@ -0,0 +1,39 @@
+import * as json2csv from 'json2csv'
+import * as uuid from 'uuid'
+import * as fs from 'fs'
+
+const fields = [
+    '_id',
+    'hat',
+    'title',
+    'text',
+    'author',
+    'img',
+    'publishDate',
+    'link',
+    'active'
+]
+
+// armazenando as fields em obj
+const opts = { fields }
+
+class ExportFiles {
+    tocsv = function (news) {
+        try {
+            const csv = json2csv.parseAsync(news, opts)
+            const filename = uuid.v4() + ".csv"
+
+            fs.writeFile("./exports/" + filename, csv, function (err) {
+                if (err) throw err
+                console.log("Arquivo exportado com sucesso")
+            })
+
+            return filename
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
+
+export default new ExportFiles()
